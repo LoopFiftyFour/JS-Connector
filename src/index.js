40c60a5018323266e6722ceb9f1e861c495df13f
Alex Woll <alexander@woll.se>
2016-10-18 15:45:14 +0200

    case #4586 remove use25Url & convertV22Response since they are not used anymore. Update tests to reflect it.

--- src/index.js
+++ src/index.js
@@ -1,131 +1,77 @@
 import axios from 'axios';
-
 import cookies from './cookies.js';
 
-import comp from './combabillity.js';
-
 let Loop54 = {
+  config: {
+    libVersion: '1.1.0',
+    url: 'No URL set for Loop54 server.'
+  },
 
-    config: {
-      libVersion: '1.0.0',
-      use25Url: false,
-      url: 'No URL set for Loop54 server.'
-    },
+  setConfig: function(newConfig) {
+    this.config = { ...this.config, ...newConfig };
+  },
 
-    setConfig: function (conf) {
-      this.config = {...this.config, ...conf};
-    },
+  getRandomUserId: function() {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 10; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    cookies.set('Loop54User', text, 365);
+    return text;
+  },
 
-    getRandomUserId: () => {
-        
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  getUserId: function() {
+    var existing = cookies.get('Loop54User');
+    if (existing) {
+      return existing;
+    }
+    return this.getRandomUserId();
+  },
 
-        for (var i = 0; i < 10; i++)
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
+  getRequestObj: function(params) {
+    let requestObj = {};
+    if (!this.CreateUserId && !params.UserId && !params.userId) {
+      params.UserId = this.getUserId();
+    }
 
-        cookies.set("Loop54User", text, 365);
+    requestObj = { ...params };
+    delete requestObj.QuestName;
+    return JSON.stringify(requestObj);
+  },
 
-        return text;
+  getEngineUrl: function(req) {
+    let url = this.config.url;
+    url = url + (url[url.length - 1] === '/' ? '' : '/');
+    return url + req.QuestName;
+  },
 
-    },
+  getResponse: function(req) {
+    const requestObj = this.getRequestObj(req);
+    const engineUrl = this.getEngineUrl(req);
 
-    getUserId: function () {
+    let promise = axios.post(engineUrl, requestObj)
+    .then(function(response) {
+      var data = response.data;
+      var responseObj = {
+        success: !!data.Success,
+      };
 
-      var existing = cookies.get("Loop54User");
-
-      if (existing) {
-          return existing;
-      }
-
-      return this.getRandomUserId();
-    },
-    
-    getRequestObj: function (params) {
-
-      let requestObj = {};
-
-      if (!this.CreateUserId && !params.UserId && !params.userId) {
-          params.UserId = this.getUserId();
-      }
-
-      //legacy mode for engines that expect the quest name to be in the JSON data
-      if (this.config.use25Url) {
-        requestObj[params.QuestName] = {...params};
-        delete requestObj[params.QuestName].QuestName;
+      if (responseObj.success) {
+        responseObj.data = data.Data;
       } else {
-        requestObj = {...params};
-        delete requestObj.QuestName;
+        responseObj.errorMessage = data.Error_Message;
       }
-
-      return JSON.stringify(requestObj);
-
-    },
-
-    getEngineUrl: function (req) {
-
-      let url = this.config.url;
-      url = url + (url[url.length - 1] === '/' ? '' : '/') ;
-
-      if(!this.config.use25Url) {
-        return url + req.QuestName;
-      }
-
-      return url;
- 
-    },
-     
-    getResponse: function (req) {
-
-      let v25Url = this.config.use25Url;
-
-      const requestObj = this.getRequestObj(req);
-      const engineUrl = this.getEngineUrl(req);
-
-      let promise = axios.post(engineUrl, requestObj)
-        .then(function (response) {
-
-          var data = response.data;
-          var responseObj = {
-            success: !!data.Success
-          };
-
-          if (!responseObj.success) {
-            responseObj.errorMessage = data.Error_Message;
-            return responseObj;
-          }
-            
-          //legacy mode for engines that return the data wrapped in the quest name
-          if(v25Url){
-            responseObj.data = data.Data[questName];
-          }
-          else
-          {
-            responseObj.data = data.Data;
-          }
-
-          comp.convertV22Response(responseObj);
-
-          return responseObj;
-        })
-        .catch(function (response) {
-          
-          var responseObj = {};
-          responseObj.success = false;
-          responseObj.errorMessage = "Connection could not be established.";
-
-          comp.convertV22Response(responseObj);
-
-          return responseObj;
-
-        });
-
-        return promise;
-    },
-
-
+      return responseObj;
+    }).catch(function(response) {
+      var responseObj = {
+        success: false,
+        errorMessage: 'Connection could not be established.',
+      };
+      return responseObj;
+    });
+    return promise;
+  },
 };
 
-
 module.exports = Loop54;
-
--- test/lib-test.js
+++ test/lib-test.js
@@ -1,22 +1,16 @@
-
 import lib from '../lib/index.js';
-
 import {assert, expect} from 'chai';
-
 
 describe('Lib', function() {
   describe('Config', function() {
     it('should have pre-set config options at init', function() {
       assert.isDefined(lib.config);
     });
-    it('should init with use25Url === false', function() {
-      assert.equal(lib.config.use25Url, false);
-    });
+
     it('should be possible to set new config options', function() {
       lib.setConfig({ test: 14 });
       expect(lib.config.test).to.equal(14);
     });
-
   });
 
   describe('Base utils', function() {
@@ -28,34 +22,14 @@ describe('Lib', function() {
       expect(lib.getUserId()).to.be.ok;
     });
 
-
-    it('should provide engine URL for v25', function() {
+    it('should provide engine URL', function() {
       var url = 'http://www.testing.com/api/';
       var questName = 'AutoComplete';
       lib.setConfig({
-        use25Url: true,
-        url,
-        questName
-      });
-      expect(lib.getEngineUrl({ questName })).to.equal(url);
-      lib.setConfig({ use25Url: false });
-    });
-
-    it('should provide engine URL for >= v26', function() {
-      var url = 'http://www.testing.com/api/';
-      var questName = 'AutoComplete';
-      lib.setConfig({
-        use25Url: false,
         url,
         questName
       });
       expect(lib.getEngineUrl({ QuestName: questName })).to.equal(url + questName);
     });
-
-
   });
-
-
-
-
 });
