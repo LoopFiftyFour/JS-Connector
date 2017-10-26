1f517b39c4eb7a65cf86aaaae4157852e8278eb7
Alex Woll <alexander@woll.se>
2017-10-26 16:53:37 +0200

    Add server functionality to provide a testing environment

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+  app = express(),
+  http = require('http'),
+  httpServer = http.Server(app);
+
+app.use(express.static(__dirname + '/lib'));
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+app.listen(3001);
+
+console.log("Test environment loaded at http://localhost:3001");
