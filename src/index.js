119b3f32ff273131d29a204fa42ebadd5fc47bf8
Alex Woll <alexander@woll.se>
2017-10-26 16:55:04 +0200

    Add new endpoints with tests

--- src/index.js
+++ src/index.js
@@ -1,82 +1,278 @@
 import axios from 'axios';
 import cookies from './cookies.js';
 
-let Loop54 = {
-  config: {
-    libVersion: '0.2.0',
+global.Loop54 = (function() {
+  var config = {
+    libVersion: '1.0.0',
     apiVersion: 'V26',
-    url: 'No URL set for Loop54 server.'
-  },
+    endpoint: 'newapi-test.54proxy.com',
+    facets: null
+  }
 
-  setConfig: function(newConfig) {
-    this.config = { ...this.config, ...newConfig };
-  },
-
-  getRandomUserId: function() {
+  var _setUserId = function() {
     var text = '';
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (var i = 0; i < 10; i++) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
-    cookies.set('Loop54User', text, 365);
+    cookies.setItem('Loop54User', text, 365);
     return text;
-  },
+  }
 
-  getUserId: function() {
-    var existing = cookies.get('Loop54User');
+  var _getUserId = function() {
+    var existing = cookies.getItem('Loop54User');
     if (existing) {
       return existing;
     }
-    return this.getRandomUserId();
-  },
+    return _setUserId();
+  }
 
-  getRequestObj: function(params) {
-    let requestObj = {};
-    if (!params.UserId && !params.userId) {
-      params.UserId = this.getUserId();
+  var _call = function(path, body, method, callback) {
+    var userId = _getUserId()
+    body = {...body}
+    var request = axios({
+      method: method ? method : 'post',
+      url: 'https://' + config.endpoint + path,
+      headers: {
+        'user-id': userId
+      },
+      responseType: 'stream',
+      data: body
+    }).then(function(response) {
+      if(response.status == 200) {
+        return {status: response.status, data: response.data}
+      } else {
+        throw new Error({error: {status: response.status, data: response.data}})
+      }
+    }).catch(function(response) {
+      return Promise.reject(response);
+    })
+
+    if(callback) {
+      request.then(callback)
+    } else {
+      return request
+    }
+  }
+
+  var _returnError = function(message, callback) {
+    if(callback) {
+      if(config.debug) { console.error(message) }
+      return callback(message)
+    } else {
+      if(config.debug) { console.error(message) }
+      return message
+    }
+  }
+
+  var _checkArguments = function(callback, args) {
+    var options = null
+    if(typeof(callback) != 'function' && args.length == 2) {
+      if(typeof(callback) == 'object' && !Array.isArray(callback)) {
+        options = callback
+        callback = null
+      } else {
+        return {
+          error: {
+            type: 'ArgumentError',
+            data: 'Bad arguments format, check your function arguments for errors',
+            errors: {
+              key: i,
+              reason: 'Not an object or object is an Array (needs to be key/value object)'
+            }
+          }
+        }
+      }
     }
 
-    requestObj = { ...params };
-    delete requestObj.QuestName;
-    return JSON.stringify(requestObj);
-  },
-
-  getEngineUrl: function(req) {
-    let url = this.config.url;
-    url = url + (url[url.length - 1] === '/' ? '' : '/');
-    return url + req.QuestName;
-  },
-
-  getResponse: function(req) {
-    const requestObj = this.getRequestObj(req);
-    const engineUrl = this.getEngineUrl(req);
-    const config = { headers: {
-      'Api-Version': this.config.apiVersion,
-      'Lib-Version': 'js:' + this.config.libVersion,
-    }};
-
-    let promise = axios.post( engineUrl, requestObj, config )
-    .then(function(response) {
-      var data = response.data;
-      var responseObj = {
-        success: !!data.Success,
-      };
-
-      if (responseObj.success) {
-        responseObj.data = data.Data;
-      } else {
-        responseObj.errorMessage = data.Error_Message;
+    if(typeof(callback) != 'function' && args.length > 2) {
+      var errors = []
+      for(var i=0;i<args.length;i++) {
+        if(i == 1) {
+          if(typeof(args[i]) == 'object' && !Array.isArray(args[i])) {
+            options = args[i]
+          } else { errors.push({key: i, reason: 'Not an object or object is an Array (needs to be key/value object)'}) }
+        }
+        if(i == 2) {
+          if(typeof(args[i]) == 'function') {
+            callback = args[i]
+          } else {
+            callback = null
+            errors.push({key: i, reason: 'Not a function'})
+          }
+        }
       }
-      return responseObj;
-    }).catch(function(response) {
-      var responseObj = {
-        success: false,
-        errorMessage: 'Connection could not be established.',
-      };
-      return responseObj;
-    });
-    return promise;
-  },
-};
 
-module.exports = Loop54;
+      if(errors.length > 0) {
+        return _returnError({error: {type: 'ArgumentError', data: 'Bad arguments format, check your search() arguments for errors', errors: errors}}, callback)
+      }
+    }
+
+    return {options: options, callback: callback}
+  }
+
+  var setConfig = function(newConfig) {
+    if(!newConfig || typeof(newConfig) != 'object') { return _returnError({ type: 'ArgumentError', data: 'Arguments missing or they have the wrong format' })}
+    if(newConfig.endpoint) { newConfig.endpoint = newConfig.endpoint.replace(/^(https?):\/\/|\/(?=[^\/]*$)/g, '') }
+    config = { ...config, ...newConfig }
+  }
+
+  var getConfig = function() {
+    return config;
+  }
+
+  var autocomplete = function(searchTerm, callback) {
+    var parameters
+    var args = _checkArguments(callback, arguments)
+    if(args.error) { return _returnError(args) }
+
+    parameters = args.options ? args.options : {}
+    callback = args.callback ? args.callback : null
+
+    var req = _call('/autocomplete', {query: searchTerm, ...parameters}, null, callback)
+
+    if(!callback) {
+      // if callback is missing, return a promise
+      return req
+    }
+  }
+
+  var trackEvent = function(eventType, type, id, orderId, quantity, revenue) {
+    // purchase, addToCart, click
+
+    if(!eventType) { return _returnError("eventType needs to be set, our standard events are 'click', 'addtocart' and 'purchase'") }
+    if(!type) { return _returnError("All entities needs to have a type set, usually this is 'Product' but can basically be anything") }
+    if(!id) { return _returnError("All entities needs to have an 'id' provided, this is usually the productId") }
+
+    var event = {
+      type: eventType,
+      entity: {
+        type: type,
+        id: id
+      }
+    }
+
+    // purchase event extra attributes
+    if(orderId) { event['orderId'] = orderId }
+    if(quantity) { event['quantity'] = parseFloat(quantity) }
+    if(revenue) { event['revenue'] = parseFloat(revenue) }
+
+    _call('/createEvents', {events: [event]})
+  }
+
+  var trackEvents = function(events) {
+    // purchase, addToCart, click
+
+    _call('/createEvents', {events: events})
+  }
+
+  var relatedEntities = function(entity, callback) {
+    /*
+    *
+    *     same as similar products
+    *
+    */
+    var parameters
+    var args = _checkArguments(callback, arguments)
+    if(args.error) { return _returnError(args) }
+
+    parameters = args.options ? args.options : {}
+    callback = args.callback ? args.callback : null
+
+    var req = _call('/getRelatedEntities', {entity: {...entity}, ...parameters}, null, callback)
+
+    if(!callback) {
+      // if callback is missing, return a promise
+      return req
+    }
+  }
+
+  var deleteEntities = function(arg, callback) {} // will implement later
+  var updateEntities = function(arg, callback) {} // will implement later
+
+  var getEntities = function(results, callback) {
+    var parameters
+    var args = _checkArguments(callback, arguments)
+    if(args.error) { return _returnError(args) }
+
+    parameters = args.options ? args.options : {}
+    callback = args.callback ? args.callback : null
+
+    var req = _call('/getEntities', {results: {...results}, ...parameters}, null, callback)
+
+    if(!callback) {
+      // if callback is missing, return a promise
+      return req
+    }
+  }
+
+  var productsInCategory = function(searchTerm, callback) {} // will implement later
+
+  var search = function(searchTerm, callback) {
+    var parameters = {}, options
+    var args = _checkArguments(callback, arguments)
+    if(args.error) { return _returnError(args) }
+
+    options = args.options ? args.options : {}
+    callback = args.callback ? args.callback : null
+
+    /*
+    * Make sure there is a searchTerm added and check the formatting (needs to be string)
+    */
+    if(typeof(searchTerm) != 'string' || searchTerm.length == 0) {
+      return _returnError({error: {type: 'ArgumentError', data: 'Search term is either missing or not a string'}}, callback)
+    }
+    if(options) {
+      if(options.relatedResults) {
+        parameters = {...parameters, relatedResults: options.relatedResults}
+      }
+
+      if(options.spellingSuggestions) {
+        parameters = {...parameters, spellingSuggestions: options.spellingSuggestions}
+      }
+
+      if(options.sortBy) {
+        parameters['results'] = {...parameters['results'], sortBy: options.sortBy}
+      }
+
+      if(options.filter) {
+        parameters['results'] = {...parameters['results'], filter: options.filter}
+      }
+
+      if(options.facets && Array.isArray(options.facets)) {
+        parameters['results'] = {...parameters['results'], facets: options.facets}
+      } else if(config.facets && Array.isArray(config.facets)) {
+        parameters['results'] = {...parameters['results'], facets: config.facets}
+      }
+
+      if(options.selectedFacets && typeof(options.selectedFacets) == 'object') {
+        if(parameters['results'] && parameters['results'].facets) {
+          for(var i=0; i<parameters['results'].facets.length; i++) {
+            parameters['results'].facets[i] = {...parameters['results'].facets[i], selected: options.selectedFacets[parameters['results'].facets[i].name]}
+          }
+        }
+      }
+
+      if(options.customData) {
+        parameters = {...parameters, customData: options.customData}
+      }
+    }
+
+    var req = _call('/search', {query: searchTerm, ...parameters}, null, callback)
+
+    if(!callback) { // if callback is missing, return a promise
+      return req
+    }
+  }
+
+  return {
+    setConfig: setConfig,
+    getConfig: getConfig,
+    autocomplete: autocomplete,
+    relatedEntities: relatedEntities,
+    getEntities: getEntities,
+    search: search,
+    trackEvent: trackEvent,
+    trackEvents: trackEvents
+  }
+})();
--- test/lib-test.js
+++ test/lib-test.js
@@ -1,35 +1,190 @@
-import lib from '../lib/index.js';
-import {assert, expect} from 'chai';
+import '../src/index.js'
+import axios from 'axios'
+import chai, {assert, expect} from 'chai'
+import nock from 'nock'
+import searchResponse from './mocks/search-response-ok'
+import autocompleteResponse from './mocks/autocomplete-response-ok'
+import relatedEntitiesResponse from './mocks/relatedEntities-response-ok'
+import getEntitiesResponse from './mocks/getEntities-response-ok'
 
-describe('Lib', function() {
+describe('Loop54', function() {
   describe('Config', function() {
     it('should have pre-set config options at init', function() {
-      assert.isDefined(lib.config);
-    });
+      expect(Loop54.getConfig()).to.include({apiVersion: 'V26'})
+    })
+  })
+  describe('.setConfig', function() {
+    it('should return error if missing arguments', function() {
+      expect(Loop54.setConfig()).to.include({type: 'ArgumentError'})
+    })
 
     it('should be possible change config options', function() {
-      lib.setConfig({ test: 14 });
-      expect(lib.config.test).to.equal(14);
-    });
-  });
+      Loop54.setConfig({ endpoint: 'https://api.test.com/' })
+      expect(Loop54.getConfig().endpoint).to.equal('api.test.com')
+    })
+  })
 
-  describe('Base utilities', function() {
-    it('should generate a random userId', function() {
-      expect(lib.getRandomUserId()).to.not.equal(lib.getRandomUserId());
-    });
+  describe('.search', function() {
+    let sandbox
+    beforeEach(() => {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      nock('https://test.loop54.se').post('/search').reply(200, searchResponse)
+    })
 
-    it('should provide a userId', function() {
-      expect(lib.getUserId()).to.be.ok;
-    });
+    it('Returns a Promise when no callback is set', function() {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      return Loop54.search('boll').then(response => {
+        expect(response.status).to.equal(200)
+        expect(response.data.results.count).to.equal(24)
+      })
+    })
 
-    it('should provide engine URL', function() {
-      var url = 'http://www.testing.com/api/';
-      var questName = 'AutoComplete';
-      lib.setConfig({
-        url,
-        questName
-      });
-      expect(lib.getEngineUrl({ QuestName: questName })).to.equal(url + questName);
-    });
-  });
-});
+    it('Accepts options as second argument without a callback', function() {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      return Loop54.search('boll', {}).then(response => {
+        expect(response.status).to.equal(200)
+        expect(response.data.results.count).to.equal(24)
+      })
+    })
+
+    it('Uses callback when given one', function(done) {
+      Loop54.search('boll', function(response) {
+        setTimeout(function() {
+          expect(response.status).to.equal(200)
+          expect(response.data.results.count).to.equal(24)
+          done()
+        }, 0)
+      })
+    })
+
+    it('Returns error if arguments are wrongly formatted', function() {
+      expect(Loop54.search('boll', [])).to.include.keys('error')
+    })
+  })
+
+  describe('.autocomplete', function() {
+    let sandbox
+    beforeEach(() => {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      nock('https://test.loop54.se').post('/autocomplete').reply(200, autocompleteResponse)
+    })
+
+    it('Returns a Promise when no callback is set', function() {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      return Loop54.autocomplete('le').then(response => {
+        expect(response.status).to.equal(200)
+        expect(response.data.queries.count).to.equal(1)
+      })
+    })
+
+    it('Accepts options as second argument without a callback', function() {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      return Loop54.autocomplete('le', {}).then(response => {
+        expect(response.status).to.equal(200)
+        expect(response.data.queries.count).to.equal(1)
+      })
+    })
+
+    it('Uses callback when given one', function(done) {
+      Loop54.autocomplete('le', function(response) {
+        setTimeout(function() {
+          expect(response.status).to.equal(200)
+          expect(response.data.queries.count).to.equal(1)
+          done()
+        }, 0)
+      })
+    })
+
+    it('Returns error if arguments are wrongly formatted', function() {
+      expect(Loop54.autocomplete('le', [])).to.include.keys('error')
+    })
+  })
+
+  describe('.relatedEntities', function() {
+    let sandbox
+    beforeEach(() => {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      nock('https://test.loop54.se').post('/getRelatedEntities').reply(200, relatedEntitiesResponse)
+    })
+
+    it('Returns a Promise when no callback is set', function() {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      return Loop54.relatedEntities({type: "Product", id: "26707104"}).then(response => {
+        expect(response.status).to.equal(200)
+        expect(response.data.results.count).to.equal(15)
+      })
+    })
+
+    it('Accepts options as second argument without a callback', function() {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      return Loop54.relatedEntities({type: "Product", id: "26707104"}, {}).then(response => {
+        expect(response.status).to.equal(200)
+        expect(response.data.results.count).to.equal(15)
+      })
+    })
+
+    it('Uses callback when given one', function(done) {
+      Loop54.relatedEntities({type: "Product", id: "26707104"}, function(response) {
+        setTimeout(function() {
+          expect(response.status).to.equal(200)
+          expect(response.data.results.count).to.equal(15)
+          done()
+        }, 0)
+      })
+    })
+
+    it('Returns error if arguments are wrongly formatted', function() {
+      expect(Loop54.relatedEntities({type: "Product", id: "26707104"}, [])).to.include.keys('error')
+    })
+  })
+
+  describe('.getEntities', function() {
+    let sandbox
+    beforeEach(() => {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      nock('https://test.loop54.se').post('/getEntities').reply(200, getEntitiesResponse)
+    })
+
+    it('Returns a Promise when no callback is set', function() {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      return Loop54.getEntities({to: 10}).then(response => {
+        expect(response.status).to.equal(200)
+        expect(response.data.results.count).to.equal(1924)
+      })
+    })
+
+    it('Accepts options as second argument without a callback', function() {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      return Loop54.getEntities({to: 10}, {}).then(response => {
+        expect(response.status).to.equal(200)
+        expect(response.data.results.count).to.equal(1924)
+      })
+    })
+
+    it('Uses callback when given one', function(done) {
+      Loop54.getEntities({to: 10}, function(response) {
+        setTimeout(function() {
+          expect(response.status).to.equal(200)
+          expect(response.data.results.count).to.equal(1924)
+          done()
+        }, 0)
+      })
+    })
+
+    it('Returns error if arguments are wrongly formatted', function() {
+      expect(Loop54.getEntities({to: 10}, [])).to.include.keys('error')
+    })
+  })
+
+  describe('.trackEvent', function() {
+    let sandbox
+    beforeEach(() => {
+      Loop54.setConfig({ endpoint: 'test.loop54.se' })
+      nock('https://test.loop54.se').post('/createEvents').reply(204)
+    })
+
+    it('Returns error if arguments are wrongly formatted', function() {
+      expect(Loop54.trackEvent()).to.include('eventType needs to be set')
+    })
+  })
+})
