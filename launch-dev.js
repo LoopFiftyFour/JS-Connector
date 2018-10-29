b4625a8be1b5c466b0b556f5a7c5e520b021746a
Christoffer Stjernlöf <christoffer@loop54.com>
2018-10-29 13:30:02 +0100

    hdZub8Hy: Execute npm run bundle before watching in launch-dev.js
    Summary:
    The watcher does not register a cold repository with no previous build as any
    change to the source files, and therefore does not trigger a rebuild. This
    leaves the main library unbuilt if `npm run dev` is executed in a freshly cloned
    repository.
    
    By running bundle first, we get the library js file generated first, and then
    we can watch for changes.
    
    Test Plan:
    Remove the build artifacts with `rm -rf lib`, then start the dev
    server with `npm run dev` and verify by inspector → network tab that the library
    does not 404.
    
    Reviewers: JoelKall, veronica
    
    Reviewed By: JoelKall
    
    Tags: #js
    
    Differential Revision: https://phabricator.loop54.com/D8

--- launch-dev.js
+++ launch-dev.js
@@ -2,22 +2,12 @@ const runAll = require("npm-run-all");
 const forever = require("forever");
 
 console.log("Starting dev environment");
-setTimeout(function() {
-    runAll(["watch"], {parallel: true, stdout: process.stdout})
-        .then(() => {
-            console.log("watching!");
-        })
-        .catch(err => {
-            console.log("watch failed!", err);
-        });
-    },0)
 
-setTimeout(function() {
-    runAll(["server"], {parallel: true, stdout: process.stdout})
-        .then(() => {
-            console.log("server done!");
-        })
-        .catch(err => {
-            console.log("server failed!", err);
-        });
-    },1000)
+const runopts = {parallel: true, stdout: process.stdout};
+
+runAll(["bundle"], runopts)
+    .then(() => runAll(["watch"], runopts))
+    .then(() => runAll(["server"], runopts))
+    .then(() => console.log("server done!"))
+    .catch(console.error);
+
